ace024d57f7993566d6e9a97b95b2797eb7bca99
sunmouren <sunxuechao1024@gmail.com>
2019-03-19 19:37:53 +0800

    delete banner overview

--- banners/views.py
+++ banners/views.py
@@ -10,11 +10,10 @@ class UploadBanner(LoginRequiredMixin, View):
     def post(self, request):
         if request.is_ajax():
             title = request.POST.get('title', None)
-            overview = request.POST.get('overview', None)
             img_url = request.POST.get('srcUrl', None)
-            if title and overview and img_url:
+            if title and img_url:
                 try:
-                    new_banner = Banner(title=title, overview=overview)
+                    new_banner = Banner(title=title, overview='test')
                     new_banner.img_url = settings.DOMAIN + img_url
                     new_banner.save()
                     return JsonResponse({'msg': 'ok'})
